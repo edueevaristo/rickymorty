3eef2fc2e366b0a13b362396038428bcbcd93e21
edueevaristo <eduevaristo_dev@hotmail.com>
2024-04-30 20:38:09 -0300

    [feature] - Commit Implementação | Realizando algumas melhorias

--- src/App.vue
+++ src/App.vue
@@ -5,19 +5,19 @@ import {ref} from 'vue';
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg navbar  bg-primary">
+  <nav class="navbar navbar-expand-lg navbar navbar-projeto">
     <div class="container-fluid">
-      <router-link class="navbar-brand" to="/">Rick and Morty</router-link>
+      <router-link class="navbar-brand" to="/"><img src="./assets/rick.png" width="100"></router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
-            <router-link class="nav-link" aria-current="page" to="/">Início</router-link>
+            <router-link class="nav-link" aria-current="page" to="/"><h4 class="navbar-text">Início</h4></router-link>
           </li>
           <li class="nav-item">
-            <router-link class="nav-link" aria-current="page" to="/about">Sobre</router-link>
+            <router-link class="nav-link" aria-current="page" to="/about"><h4 class="navbar-text">Sobre</h4></router-link>
           </li>
         </ul>
       </div>
--- src/components/listPersonagensRickyMorty.vue
+++ src/components/listPersonagensRickyMorty.vue
@@ -6,29 +6,30 @@
 
 <template>
 
-    <div class="card mb-3 ms-5 card-personagens card-hover" style="">
+    <div class="card mb-3 ms-5 card-personagens card-hover" style="" href="#">
         <div class="row g-0">
             <div class="col-md-4">
                 <img :src=personagem.image class="img-fluid rounded-start mt-5" alt="Imagem do personagem">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
-                    <h5 class="card-title">{{ personagem.name }}</h5>
-                    Status:
+                    <h5 class="card-title text-white">{{ personagem.name }}</h5>
+                    <span class="text-white">Status: </span>
                     <span v-if="personagem.status === 'Alive'" class="badge text-bg-success">{{ personagem.status }}</span>
                     <span v-else-if="personagem.status === 'Dead'" class="badge text-bg-danger">{{ personagem.status }}</span>
                     <span v-else-if="personagem.status === 'unknown'" class="badge text-bg-warning">{{ personagem.status }}</span>
                     <span v-else class="badge text-bg-danger">{{ personagem.status }}</span>
                     
-                    <h6 class="card-text"><b>Espécie:</b> {{ personagem.species }} </h6>
-                    <h6 class="card-text"><b>Gênero:</b> {{ personagem.gender }}</h6>
-                    <p class="card-text"><small class="text-muted"><b>Localização Atual:</b> {{ personagem.location }}</small></p>
+                    <h6 class="card-text text-white"><b>Espécie:</b> {{ personagem.species }} </h6>
+                    <h6 class="card-text text-white"><b>Gênero:</b> {{ personagem.gender }}</h6>
+                    <p class="card-text text-white"><small class="text-white"><b>Localização Atual:</b> {{ personagem.location }}</small></p>
                     <span class="badge text-bg-success"><b>Total de episódios:</b> {{ personagem.episode.length }}</span>
 
                 </div>
             </div>
         </div>
     </div>
+    
 
 </template>
 
